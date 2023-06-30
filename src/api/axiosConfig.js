cbe577b7013050f464c616385824c5f536968be5
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-06-30 14:42:56 -0700

    point to ec2 spring boot

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+	// baseURL: "http://localhost:8080",
+	baseURL: "http://18.144.53.10:8080", // to point to ec2. Should change to elastic ip.
+	headers: { "Content-Type": "application/json" },
+	withCredentials: true,
 });
