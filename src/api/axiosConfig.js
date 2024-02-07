0bf6602df60a1431b01af2a6168d572f05b5635f
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-02-07 10:55:33 -0800

    config axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "http://localhost:8080",
+	baseURL: "http://localhost:8080", // local spring boot
+	baseURL: "http://localhost:5000", // local node
 	// baseURL: "http://18.144.53.10:8080", // to point to ec2 spring boot. Should change to elastic ip.
+	// baseURL: "http://18.144.53.10:5000", // to point to ec2 node. Should change to elastic ip.
 	headers: { "Content-Type": "application/json" },
 	withCredentials: true,
 });
